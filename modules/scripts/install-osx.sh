64d81011f60c72876d38e8aee87b99a25a3c169e
Jiayuan Mao <maojiayuan@gmail.com>
2018-03-21 17:45:36 +0800

    [master] update install scripts

--- modules/scripts/install-osx.sh
+++ modules/scripts/install-osx.sh
@@ -1,8 +1,13 @@
+echo "Install programming languages"
+brew install cmake pkg-config python3 go node perl ruby rust swig bazel jq imagemagick
+
+brew install tbb tcl-tk
+
+echo "Install system tools"
+brew install ssh-copy-id zsh tmux ranger tree unrar fontconfig curl wget axel aria2 the_silver_searcher watch htop nload
+
+echo "Install vim with python3 support"
+brew install vim --with-python3
+
 echo "Install quick look plugins"
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
-
-echo "Install zsh tmux"
-brew install zsh tmux 
-
-echo "Install system utilities"
-brew install htop nload ctags python3

dbedc35b635374bfd9b5d2f3f1b8b208a29f769c
Jiayuan Mao <maojiayuan@gmail.com>
2018-04-20 12:03:18 +0800

    [master] add custom templates

---
+++ modules/vim/vim/templates/=template=.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File   : %FFILE%
+# Author : %USER%
+# Email  : %MAIL%
+# Date   : %MONTH%/%DAY%/%YEAR%
+# 
+# Distributed under terms of the %LICENSE% license.
+
+"""
+%HERE%
+"""

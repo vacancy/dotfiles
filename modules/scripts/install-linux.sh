7958b40a93c60ac94710cd024b4d6939aecaca22
Jiayuan Mao <maojiayuan@gmail.com>
2021-08-18 17:55:42 -0400

    [master] fix install linux

--- modules/scripts/install-linux.sh
+++ modules/scripts/install-linux.sh
@@ -6,10 +6,11 @@ if [[ "$(whereis sudo)" == *'/'* && "$(sudo -nv 2>&1)" != 'Sorry, user'* ]]
 then
     sudo locale-gen en_US.UTF-8
     sudo apt-get update
+    sudo apt-get install git zsh --yes
     sudo apt-get install build-essential cmake --yes
-    sudo apt-get install git tmux zsh vim htop nload ctags nodejs-dev npm golang --yes
+    sudo apt-get install tmux vim htop nload ctags golang --yes
     sudo apt-get install silversearcher-ag axel feh --yes
-    sudo apt-get install python2.7-dev python3.5-dev --yes
+    sudo apt-get install python2.7-dev python3-dev --yes
     sudo apt-get install python-setuptools python3-setuptools --yes
 else
     echo "You are not a sudoer."
